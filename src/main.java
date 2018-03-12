441e76c174de8c5d001573e0125f9a799112a3bd
VTWR6254 <VTWR6254@EB-OR6125078.lille.francetelecom.fr>
2018-03-12 14:14:01 +0100

    12/03/2018 - Hugo Rajout du Switch - Case

--- src/main.java
+++ src/main.java
@@ -5,11 +5,33 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		int x;
 		
 		System.out.println("On est des génies");
 		System.out.println("\n\t Merci de rentrer le message à coder ou à décoder\n");
 		String Entree_clavier = sc.nextLine();
 		System.out.println("Merci, vous avez rentré :\t" + Entree_clavier);
+		
+		do {
+			System.out.println("Rentrez une valeur svp\n(1) pour convertir du morse ou (2) pourconvertir en morse");
+			Scanner choix = new Scanner(System.in);
+			x = choix.nextInt();
+		}while((x!=1)&&(x!=2));
+		
+		switch(x) {
+			case 1:
+				System.out.println("Vous avez rentré 1");
+				break;
+				
+			case 2:
+				System.out.println("Vous avez rentré 2");
+				break;
+				
+			default:												//Prévoir au cas où une valeur autre que 1 ou 2 est entrée et que la boucle n'a pas fonctionné
+				System.out.println("Une erreur est intervenue");
+		}
+		
+		
 	}
 
 }
